2956ce3dfc2538a0d47b553ddc7c2c13ed7abb3b
torbjorn rasmusson <torbjorn.rasmusson@gmail.com>
2021-12-13 19:22:44 +0100

    First funktioning firmware

---
+++ keymaps/toddis1/keymap.c
@@ -0,0 +1,26 @@
+// Copyright 2021 torbjorn rasmusson (@torbjorn rasmusson)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _FN
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT(
+        KC_A,    KC_B, KC_C, KC_D, KC_E, KC_F,
+        KC_A,    KC_B, KC_C, KC_D, KC_E, KC_F,
+        KC_A,    KC_B, KC_C, KC_D, KC_E, KC_F,
+            KC_A,   KC_B, KC_D
+    ),
+    [_FN] = LAYOUT(
+        KC_A,    KC_B, KC_C, KC_D, KC_E, KC_F,
+        KC_A,    KC_B, KC_C, KC_D, KC_E, KC_F,
+        KC_A,    KC_B, KC_C, KC_D, KC_E, KC_F,
+            KC_A,   KC_B, KC_D
+    )
+};
